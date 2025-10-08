2c7a15abafca6658b96dbebc2ba3a1269a4c526f
Danield19z <dtorres@iesjulioantonio.cat>
2025-10-08 08:38:07 +0200

    .

--- R2/2.2 ControlFlux 2/RA2_2.2_9_DanielTorres.py
+++ R2/2.2 ControlFlux 2/RA2_2.2_9_DanielTorres.py
@@ -2,11 +2,11 @@
 #Data: 02/10/2023
 #Versio: 2.1
 #Trobar el número màxim d’una llista
-for i in range(5):
-    num = int(input("Introdueix un número: "))
-    if i == 0:
-        max_num = num
-    else:
-        if num > max_num:
+for i in range(5): #Funciona per a 5 números
+    num = int(input("Introdueix un número: ")) #Variable per a guardar el número
+    if i == 0: #Funcio per a que el primer número sigui el màxim
+        max_num = num #Variable per a guardar el número màxim
+    else: #Funcio per a comparar els números
+        if num > max_num: 
             max_num = num
-print("El número màxim és:", max_num)
+print("El número màxim és:", max_num) #Mostra el número màxim
