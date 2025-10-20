e5015bad318008ea3ee462253de6899101def7be
Danield19z <dtorres@iesjulioantonio.cat>
2025-10-20 09:11:16 +0200

    .

--- R2/2.3 ControlFlux 3/RA2_2.3_3_DanielTorres.py
+++ R2/2.3 ControlFlux 3/RA2_2.3_3_DanielTorres.py
@@ -1,3 +1,10 @@
 #Programa fet per Daniel Torres
 #Data: 17/10/2023
 #Versio: 2.1
+#Un programa que mostri la taula de multiplicar d'un número introduït per l'usuari
+try:
+    num=int(input("Introdueix un número:"))
+    for i in range(1,11):
+        print(num,"*",i,"=",num*i)
+except ValueError:
+    print("Si us plau, introdueix un número vàlid.")
