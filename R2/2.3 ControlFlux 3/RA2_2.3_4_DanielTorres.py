2f43b04ad48bb17fe13e82caff48212c52998c3b
Danield19z <dtorres@iesjulioantonio.cat>
2025-10-21 08:53:18 +0200

    .

--- R2/2.3 ControlFlux 3/RA2_2.3_4_DanielTorres.py
+++ R2/2.3 ControlFlux 3/RA2_2.3_4_DanielTorres.py
@@ -1,3 +1,14 @@
 #Programa fet per Daniel Torres
 #Data: 17/10/2023
 #Versio: 2.1
+#Demana a l'usuari un nombre enter i imprimeix tots els nombres parells des de 0 fins a aquest nombre
+try:
+    num=int(input("Introdueix un nombre enter: "))
+    if num < 0:
+        print("introdueix un nombre enter no negatiu.")
+    else:
+        print("Nombres parells des de 0 fins a", num, ":")
+        for i in range(0, num + 1, 2):
+            print(i)
+except ValueError:
+    print("Si us plau, introdueix un nombre enter vàlid.")
--- R2/2.3 ControlFlux 3/RA2_2.3_5_DanielTorres.py
+++ R2/2.3 ControlFlux 3/RA2_2.3_5_DanielTorres.py
@@ -1,3 +1,14 @@
 #Programa fet per Daniel Torres
 #Data: 17/10/2023
 #Versio: 2.1
+#Demana a l'usuari un nombre enter i imprimeix tots els nombres primers des de 2 fins a aquest nombre
+try:
+    num = int(input("Introdueix un nombre enter: "))
+    for i in range(2, num + 1):
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                break
+        else:
+            print(i)
+except ValueError:
+    print("Has d'introduir un nombre enter vàlid.")
