514bcd308f43c9f71fb76f7c1bef87ea5855e543
Danield19z <dtorres@iesjulioantonio.cat>
2025-10-21 13:04:19 +0200

    .

--- R2/2.3 ControlFlux 3/RA2_2.3_5_DanielTorres.py
+++ R2/2.3 ControlFlux 3/RA2_2.3_5_DanielTorres.py
@@ -3,12 +3,19 @@
 #Versio: 2.1
 #Demana a l'usuari un nombre enter i imprimeix tots els nombres primers des de 2 fins a aquest nombre
 try:
-    num = int(input("Introdueix un nombre enter: "))
-    for i in range(2, num + 1):
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                break
-        else:
-            print(i)
+    num = int(input("Introdueix un nombre enter positiu: "))
+    if num < 2:
+        print("Si us plau, introdueix un nombre enter major o igual a 2.")
+    else:
+        print(f"Nombres primers des de 2 fins a {num}:")
+        for n in range(2, num + 1):
+            es_primer = True
+            for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                    es_primer = False
+                    break
+            if es_primer:
+                print(n, end=' ')
+        print()  # Nova línia després de la llista de nombres primers
 except ValueError:
-    print("Has d'introduir un nombre enter vàlid.")
+    print("Entrada no vàlida. Si us plau, introdueix un nombre enter.")
