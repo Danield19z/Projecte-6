640a9361572073b103034c1dde8c240143f26f84
Danield19z <dtorres@iesjulioantonio.cat>
2025-11-07 11:24:54 +0100

    .

--- R2/2.5 FUNCIONS I MÒDULS/PRACTICA LES FUNCIONS/RA2_2.5_9_DanielTorres.py
+++ R2/2.5 FUNCIONS I MÒDULS/PRACTICA LES FUNCIONS/RA2_2.5_9_DanielTorres.py
@@ -2,13 +2,12 @@
 #Data: 07/11/2025
 #Versio: 2.1
 #Determina l'estat de diferents edats
+edat = int(input("posa edat: "))
 def estat_persona(edat):
     if edat <= 12:
         return "Infantil"
-    elif edat <= 25:
+    elif edat >= 18:
         return "Adult"
-    elif edat <= 70:
+    elif edat >= 70:
         return "Abuelo"
-print(estat_persona(12))
-print(estat_persona(25))
-print(estat_persona(70))
+print (estat_persona(edat)) 
