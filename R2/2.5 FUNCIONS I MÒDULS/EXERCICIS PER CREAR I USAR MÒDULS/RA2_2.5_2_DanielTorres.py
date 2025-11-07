ac430d0d528617c8f50ebfb069bdec03d0561c41
Danield19z <dtorres@iesjulioantonio.cat>
2025-11-07 18:24:35 +0100

    Delete R2/2.5 FUNCIONS I MÒDULS/EXERCICIS PER CREAR I USAR MÒDULS/RA2_2.5_2_DanielTorres.py

--- R2/2.5 FUNCIONS I MÒDULS/EXERCICIS PER CREAR I USAR MÒDULS/RA2_2.5_2_DanielTorres.py
+++
@@ -1,7 +0,0 @@
-#Programa fet per Daniel Torres
-#Data: 07/11/2025
-#Versio: 2.1
-#Conversió de temperatures
-import conversions
-print (input("Posa una temperatura: "),conversions.celsius_a_fahrenheit)
-print (input("Posa una temperatura: "), conversions.fahrenheit_a_celsius)
