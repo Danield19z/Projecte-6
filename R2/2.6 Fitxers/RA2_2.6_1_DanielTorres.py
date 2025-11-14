3d92ec7616c470c7f5cf4294910de153c00d3052
Danield19z <dtorres@iesjulioantonio.cat>
2025-11-14 20:08:14 +0100

    Delete R2/2.6 Fitxers/RA2_2.6_1_DanielTorres.py

--- R2/2.6 Fitxers/RA2_2.6_1_DanielTorres.py
+++
@@ -1,8 +0,0 @@
-#Programa fet per Daniel Torres
-#Data: 14/11/2025
-#Versio: 2.1
-#Escriu un programa que llegeixi i mostri el contingut per pantalla
-fitxer = open("missatge.txt", "r")
-contingut = fitxer.read()
-fitxer.close
-print(contingut)
