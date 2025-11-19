72d39b67d420486e6dbff90fddb0c933156a8d20
Danield19z <dtorres@iesjulioantonio.cat>
2025-11-19 09:15:35 +0100

    Delete R2/2.6  Fitxers/RA2_2.6_4_DanielTorres.py

--- R2/2.6  Fitxers/RA2_2.6_4_DanielTorres.py
+++
@@ -1,8 +0,0 @@
-#Programa fet per Daniel Torres
-#Data: 14/11/2025
-#Versio: 2.1
-#Llegeix un fitxer i mostra quantes línies té
-fitxer = open("sortida.txt", "r")  
-linies = fitxer.readlines()         
-print("El fitxer té", len(linies), "línies.") 
-fitxer.close()                      
