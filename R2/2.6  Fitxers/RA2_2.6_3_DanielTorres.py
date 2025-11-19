007287ae54a659ab1181f3a3a50b266ffd4b9516
Danield19z <dtorres@iesjulioantonio.cat>
2025-11-19 09:15:28 +0100

    Delete R2/2.6  Fitxers/RA2_2.6_3_DanielTorres.py

--- R2/2.6  Fitxers/RA2_2.6_3_DanielTorres.py
+++
@@ -1,7 +0,0 @@
-#Programa fet per Daniel Torres
-#Data: 14/11/2025
-#Versio: 2.1
-#Afegeix una línia nova a un fitxer existent sortida.txt
-fitxer = open("sortida.txt", "a")
-fitxer.write("Aquesta és una línia afegida\n")
-fitxer.close()
