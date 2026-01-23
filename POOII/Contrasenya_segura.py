3ffdeb85078353175ffc0caedb4a195a9f318e47
Danield19z <dtorres@iesjulioantonio.cat>
2026-01-23 12:55:05 +0100

    Update Contrasenya_segura.py

--- POOII/Contrasenya_segura.py
+++ POOII/Contrasenya_segura.py
@@ -2,26 +2,20 @@
 #Data: 21/01/2026
 #Versio: 2.1
 #Crea una clasee que lo que fa es verificar la contraseña i pots cambiar la contraseña
-class ContrasenyaSegura:
+class Usuari:
     def __init__(self, contrasenya):
         self.__contrasenya = contrasenya
 
     def canviar_contrasenya(self, nova):
         if len(nova) >= 8:
             self.__contrasenya = nova
+            return True, "Contrasenya canviada correctament"
+        else:
+            return False, "La contrasenya ha de tenir almenys 8 caràcters"
 
     def verificar_contrasenya(self, clau):
         return clau == self.__contrasenya
 
-class ContrasenyaSegura:
-    def __init__(self, contrasenya):
-        self.__contrasenya = contrasenya
 
-    def canviar_contrasenya(self, nova):
-        if len(nova) >= 8:
-            self.__contrasenya = nova
-
-    def verificar_contrasenya(self, clau):
-        return clau == self.__contrasenya
 
 
